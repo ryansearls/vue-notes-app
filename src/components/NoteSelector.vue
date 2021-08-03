c9bfbbbf92f0a995c76163da71ffffea07b34782
Ryan Searls <Ryan.searls@gmail.com>
2021-08-03 13:51:00 -0500

    added note data

--- src/components/NoteSelector.vue
+++ src/components/NoteSelector.vue
@@ -1,16 +1,14 @@
 <template>
   <div>
     <div class="note-selector active">
-      <p class="note-selector-title">First note...</p>
-      <p class="note-selector-timestamp">Timestamp here...</p>
-    </div>
-    <div class="note-selector">
-      <p class="note-selector-title">Second note...</p>
-      <p class="note-selector-timestamp">Timestamp here...</p>
-    </div>
-    <div class="note-selector">
-      <p class="note-selector-title">Third note...</p>
-      <p class="note-selector-timestamp">Timestamp here...</p>
+      <p class="note-selector-title">{{ note.body }}</p>
+      <p class="note-selector-timestamp">{{ note.timestamp }}</p>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: ["note"],
+};
+</script>
